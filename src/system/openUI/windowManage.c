74be26c949a190866651e4992641d35ddb30d19c
D4B2A <128427718+D4B2A@users.noreply.github.com>
2024-01-27 17:34:36 +0100

    Update windowManage.c

--- src/system/openUI/windowManage.c
+++ src/system/openUI/windowManage.c
@@ -2,18 +2,17 @@
 
 #define WINDOW_LIMIT 64
 
-static struct screenApplication[WINDOW_LIMIT] areaBuffer;
+static struct screenApplication[WINDOW_LIMIT] windowBuffer;
 
 struct frame renderFrame() {
+  struct frameMap = emptyFrameMap();
   for(int i = 0; i<windowCount; i++){
-      
+      renderWindow(windowBuffer[i],*frameMap);
   }
+  return frameMap.frame;
 }
 
 
 struct frameMap emptyFrameMap() {
-  bool [resolution.x][resolution.y];
-  
-  
   return eFrameMap; 
 }
