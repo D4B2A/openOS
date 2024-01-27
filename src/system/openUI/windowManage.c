06b0db1f30925484c913aa46fe60d7aa6c0fc269
D4B2A <128427718+D4B2A@users.noreply.github.com>
2024-01-27 13:44:48 +0100

    Update windowManage.c

--- src/system/openUI/windowManage.c
+++ src/system/openUI/windowManage.c
@@ -1,3 +1,12 @@
+#include<"openUI.h">
+
 #define WINDOW_LIMIT 64
 
 static struct screenApplication[WINDOW_LIMIT] areaBuffer;
+
+void updateAreaBuffer(){ 
+  int* n;
+  for(int i = 0; i<lenght;i++){
+    
+  }
+}
